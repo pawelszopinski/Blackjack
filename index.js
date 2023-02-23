0c40b45b7d79734046be96008e7ef45749859ac3
Paweł Szopiński <szopinskipawel@gmail.com>
2023-02-23 18:09:36 +0100

    Added loop for cards array

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 let firstCard= 10;
-let secondCard= 21;
+let secondCard= 8;
+let cards = [firstCard,secondCard]
 let cardSum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-let message = "" 
-//let startEl = document.getElementById("start-el");
+let startEl = document.getElementById("start-el")
 let messageEl = document.getElementById("message-el")
 let cardsSum = document.getElementById("sum-el")
 let cardsEl =document.getElementById("cards-el")
-function start() { 
- if(cardSum <= 20) {
-     cardsEl.textContent = firstCard + " | " + secondCard;
-     messageEl.textContent = "Draw a card?"
-     cardsSum.textContent = "Sum:"+" "+cardSum;
-     }
-    else if (cardSum ===21) {
-         cardsEl.textContent = firstCard + " | " + secondCard;
-        messageEl.textContent= "Victory!!! Blackjack is here!"
-        cardsSum.textContent = "Sum:"+" "+cardSum;
-        hasBlackJack=true
-      }
+let newEl = document.querySelector("#new-el")
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent="Cards:"
+
+    for (i=0;i<cards.length;i++) {
+        cardsEl.textContent += " "+ cards[i] + " "
+    }
+
+
+        if (cardSum <= 20) {
+            messageEl.textContent = "Draw a card?";
+            cardsSum.textContent = "Sum:" + " " + cardSum;
+        }
+        else if (cardSum === 21) {
+            messageEl.textContent = "Victory!!! Blackjack is here!";
+            cardsSum.textContent = "Sum:" + " " + cardSum;
+            hasBlackJack = true;
+        }
         else {
-             cardsEl.textContent = firstCard + " | " + secondCard;
-            messageEl.textContent = "Game over. Better luck next time!"
-       cardsSum.textContent = "Sum:"+" "+cardSum;         
+            messageEl.textContent = "Game over. Better luck next time!";
+            cardsSum.textContent = "Sum:" + " " + cardSum;
             isAlive = false;
         }
-        console.log(hasBlackJack)
-    console.log(isAlive)
-    console.log(message)
-        };
+    };
+        function thirdcard () {
+                 let thirdCard = 3;
+                 cardSum +=thirdCard;
+                 cards.push(thirdCard);
+                 renderGame()
+                 
+             };
+             
